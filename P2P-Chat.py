edae381cedb0eb735b3df8ec84063faa715b0f1a
ThimoSchaub <32570143+ThimoSchaub@users.noreply.github.com>
2018-11-27 12:57:56 +0100

    Update P2P-Chat.py

--- P2P-Chat.py
+++ P2P-Chat.py
@@ -1,5 +1,6 @@
 import socket, pickle, struct
 from _thread import start_new_thread
+import threading
 import _tkinter
 from tkinter import *
 from tkinter import scrolledtext
@@ -18,39 +19,41 @@ class join:
 class exit:
     exit = "has left the chat"
 
-window = Tk()
-window.title("P2P_Group Chat")
-out = scrolledtext.ScrolledText(window,width=40,height=10)
-txt = Entry(window, width=30)
+buddylist=[]
+PORT= 50000
 name = "T(h)imo"
+
 def client():
-    HOST = 'localhost'
-    PORT = 50000
-    # Create a socket connection.
+    jointoserver('localhost')
+    exit = False
+    while not exit:
+        msg = input("Insert Message: ")
+        sendmessagetoserver('localhost',msg)
+    s.close()
+    print('Data Sent to Server')
+
+def jointoserver(ip):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    name = input("Insert name: ")
+    s.connect((ip,PORT))
     packet = pickle.dumps(join(name))
     length = struct.pack('!I', len(packet))
     packet = length + packet
     s.send(packet)
-    exit = False
-    while not exit: 
-        msg = input("Insert Message: ")
-        packet = pickle.dumps(message(msg))
-        length = struct.pack('!I', len(packet))
-        packet = length + packet
-        s.send(packet)
-    s.close()
-    print('Data Sent to Server')
 
+def sendmessagetoserver(ip,msg):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, PORT))
+    packet = pickle.dumps(message(msg))
+    length = struct.pack('!I', len(packet))
+    packet = length + packet
+    s.send(packet)
 
-def server_2():
-    HOST = 'localhost'
+def server():
+    HOST = '141.37.168.38'
     PORT = 50000
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
-    s.listen(1)
+    s.listen(12)
     conn, addr = s.accept()
     print('Connected by', addr)
 
@@ -65,7 +68,7 @@ def server_2():
             name = data_variable.name
         elif type(data_variable) is message:
             msg = data_variable.msg
-            out.insert= (name+":" + msg)
+            print(name+":" + msg)
         # elif(type(data_variable) is exit):
 
         else:
@@ -80,28 +83,40 @@ def clicked():
     client()
 
 
+def recjoin():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('141.37.168.38', PORT))
+    s.listen(12)
+    conn, addr = s.accept()
+    buf = b''
+    while len(buf) < 4:
+        buf += conn.recv(4 - len(buf))
+    length = struct.unpack('!I', buf)[0]
+    msg = conn.recv(length)
+    data_variable = pickle.loads(msg)
+    if type(data_variable) is join:
+        name = data_variable.name
+        return name
 
-def gui():
+def connect():
+    for i in range(30, 42, 1):
+        t = threading.Thread(target=scan,args=[i])
+        t.start()
 
-    out.grid(column=0,row=0,columnspan=2)
+def scan(s):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        ip = '141.37.168.' + str(s)
+        sock.connect((ip, 50000))
+        jointoserver(ip)
+        name = recjoin()
+        print('IP:'+ip+' Nickname:'+name)
+    except :
+        print("Ip "+ip+ " does not respond")
+        pass
+        # print("Fehler "+str(i)+"\r\n")
 
 
-    txt.grid(column=0, row=1)
-    btn = Button(window, text="Send", command=clicked)
-
-    btn.grid(column=1, row=1)
-    window.mainloop()
-
-
-
-start_new_thread(server_2,())
-#gui()
-client()
-# def start_server():
-#
-#
-# def scan(s):
-#
 # def list(l):
 #
 # def chat(c):
@@ -109,6 +124,17 @@ client()
 # def group_chat(g):
 #
 # def quit(Q):
+
+
+#server()
+#start_new_thread(server,())
+#gui()
+#client()
+connect()
+
+
+
+
 #
 # s.send(message.encode('utf-8'))
 # pickledResponse = s.recv(4096);
