bcdd42cf3d78ff5c625af32a06e11a113d8906ce
ThimoSchaub <32570143+ThimoSchaub@users.noreply.github.com>
2018-11-28 14:59:32 +0100

    Update P2P-Chat.py

--- P2P-Chat.py
+++ P2P-Chat.py
@@ -26,7 +26,7 @@ class exit:
 
 buddylist = []
 PORT = 50000
-name = "T(h)imo"
+name = "Timo"
 lock = threading.Lock()
 
 def getSockofBuddy(s):
@@ -41,29 +41,7 @@ def getBuddyfromIp(s):
             return True
     return False
 
-def recmessage(conn, addr,sock):
-    while True:
-        buf = b''
-        while len(buf) < 4:
-            buf += conn.recv(4 - len(buf))
-        length = struct.unpack('!I', buf)[0]
-        msg = conn.recv(length)
-        data_variable = pickle.loads(msg)
-        if type(data_variable) is join:
-            name = data_variable.name
-            print(name)
-            if not getBuddyfromIp(addr[0]):
-                lock.acquire()
-                buddylist.append((name, addr[0], sock))
-                lock.release()
-                jointoserver(addr[0])
-        elif type(data_variable) is message:
-            msg = data_variable.msg
-            print(name + ":" + msg)
-        # elif(type(data_variable) is exit):
 
-        else:
-            print("unknown message typ")
 
 
 def jointoserver(sock):
@@ -73,15 +51,15 @@ def jointoserver(sock):
         packet = length + packet
         sock.send(packet)
     except:
-        print("IP " + ip + " refused the connection")
+       print("IP " + sock + " refused the connection")
 
 
 def sendmessagetoserver(sock,msg):
     packet = pickle.dumps(message(msg))
     length = struct.pack('!I', len(packet))
     packet = length + packet
-    sock.open()
     sock.send(packet)
+
 
 def sendquittoserver(sock):
     packet = pickle.dumps(exit())
@@ -90,14 +68,10 @@ def sendquittoserver(sock):
     sock.send(packet)
 
 
-
-
-
-
 def recjoin():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('141.37.168.38', PORT))
-    s.listen(12)
+    s.listen(1)
     conn, addr = s.accept()
     buf = b''
     while len(buf) < 4:
@@ -107,6 +81,7 @@ def recjoin():
     data_variable = pickle.loads(msg)
     if type(data_variable) is join:
         name = data_variable.name
+        print("Recjoin gets name "+name)
         return name
 
 
@@ -115,19 +90,19 @@ def connect(s):
     sock.settimeout(1)
     try:
         ip = '141.37.168.' + str(s)
+        print("Try to connect to ip:"+ip)
         sock.connect((ip, 50000))
-
         jointoserver(sock)
-        name = recjoin()
-        lock.acquire()
-        buddylist.append((name,ip,sock))
-        lock.release()
-        print('IP:' + ip + ' Nickname:' + name)
+        # b_name = recjoin()
+        # lock.acquire()
+        # buddylist.append((b_name, ip, sock))
+        # lock.release()
+        #print('IP:' + ip + ' Nickname:' + b_name)
     except:
-        print("Ip " + ip + " does not respond")
-        pass
-        # print("Fehler "+str(i)+"\r\n")
+        print("Connection to " + ip + " failed")
 
+
+#<editor-fold desc="Bedienelemente">
 def scan(s):
     for i in range(s[0], s[1], 1):
         connect(i)
@@ -140,12 +115,12 @@ def list(l):
 
 def chat(c):
     msg = input("Enter  Message: ")
-    sendmessagetoserver(c,msg)
+    sendmessagetoserver(c, msg)
 
 
 def group_chat(g):
     for i in buddylist:
-        sendmessagetoserver(i[2],g)
+        sendmessagetoserver(i[2], g)
 
 
 def quit(q):
@@ -153,7 +128,7 @@ def quit(q):
         sendquittoserver(i[2])
         i[2].close()
         buddylist.remove(i)
-
+#</editor-fold>
 
 def client():
     o = input("Type q for quit\n Type c for chat \n Type g for group chat \n Type l for list\n Type s for scan \n")
@@ -165,35 +140,71 @@ def client():
         if sock is None:
             print("Buddy isn't in the buddy list\n")
         else:
-            msg= input("Type Message:\n")
-            sendmessagetoserver(sock,msg)
+           chat(sock)
     elif o == 'l':
         list(buddylist)
     elif o == 's':
-        scan([30, 42])
+        scan([37, 40])
     elif o == 'g':
         msg = input("Enter Message")
         group_chat(msg)
     client()
 
+def jointoserver(sock):
+    try:
+        packet = pickle.dumps(join(name))
+        length = struct.pack('!I', len(packet))
+        packet = length + packet
+        sock.send(packet)
+    except:
+       print("IP " + sock + " refused the connection")
 
-def next_server(s):
-    conn, addr = s.accept()
-    start_new_thread(next_server, (s,))
+
+def recmessage(newsocket, addr):
+    while True:
+        buf = b''
+        print(addr[0])
+        while len(buf) < 4:
+            buf += newsocket.recv(4 - len(buf))
+        length = struct.unpack('!I', buf)[0]
+        msg = newsocket.recv(length)
+        data_variable = pickle.loads(msg)
+        if type(data_variable) is join:
+            name = data_variable.name
+            print("Server gets name "+name)
+            if not getBuddyfromIp(addr[0]):
+                lock.acquire()
+                buddylist.append((name, addr[0], newsocket))
+                lock.release()
+                #jointoserver(conn)
+        elif type(data_variable) is message:
+            msg = data_variable.msg
+            print(name + ":" + msg)
+        # elif(type(data_variable) is exit):
+
+        else:
+            print("unknown message typ")
+
+
+def next_server(parentsocket):
+    newsocket, addr = parentsocket.accept()
+    print("Server started")
+    start_new_thread(next_server, (parentsocket,))
     print('Connected by', addr)
-    recmessage(conn, addr,s)
+    recmessage(newsocket, addr)
 
 
 
 def server():
     HOST = '141.37.168.38'
     PORT = 50000
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen(12)
-    next_server(s)
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((HOST, PORT))
+    serversocket.listen(12)
+    start_new_thread(next_server,(serversocket,))
 
-start_new_thread(server,())
+server()
+
 client()
 
 # start_new_thread(server,())
