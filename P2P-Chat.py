dd375ddf5382bfc18ad664bfc541bce5db458508
ThimoSchaub <32570143+ThimoSchaub@users.noreply.github.com>
2018-12-04 12:47:02 +0100

    Update P2P-Chat.py

--- P2P-Chat.py
+++ P2P-Chat.py
@@ -1,9 +1,9 @@
-import socket, pickle, struct
+import struct
+from socket import *
 from _thread import start_new_thread
-import threading
-import _tkinter
-from tkinter import *
-from tkinter import scrolledtext
+import pickle
+import sys
+import time
 
 
 class message:
@@ -24,200 +24,184 @@ class exit:
     exit = "has left the chat"
 
 
-buddylist = []
+class Buddy:
+    def __init__(self, name, conn, addr):
+        self.name = name
+        self.conn = conn
+        self.addr = addr
+
+    def __eq__(self, other):
+        """Override the default Equals behavior"""
+        return self.name == other.name and self.addr == other.addr
+
+
+USERNAME = "DieTimos"
+HOST = gethostbyname(gethostname())
+IPS = ["141.37.168.40", "141.37.168.41"]
+#HOST = "127.0.0.4"
 PORT = 50000
-name = "Timo"
-lock = threading.Lock()
-
-def getSockofBuddy(s):
-    for buddy in buddylist:
-        if(buddy[0]== s):
-            return buddy[2]
-    return None
-
-def getBuddyfromIp(s):
-    for buddy in buddylist:
-        if(buddy[1]== s):
-            return True
-    return False
+BUDDYS = []
+THREAD_RUNNING = True
+sock = socket(AF_INET, SOCK_STREAM)
+sock.bind((HOST, PORT))
+sock.listen(10)
 
 
-
-
-def jointoserver(sock):
-    try:
-        packet = pickle.dumps(join(name))
+def exec_join(name, conn, addr):
+    tmp = Buddy(name, conn, addr)
+    if tmp not in BUDDYS:
+        BUDDYS.append(tmp)
+        packet = pickle.dumps(join(USERNAME))
         length = struct.pack('!I', len(packet))
         packet = length + packet
-        sock.send(packet)
-    except:
-       print("IP " + sock + " refused the connection")
+        conn.send(packet)
+        print(str(tmp.name) + " connected.\n")
 
 
-def sendmessagetoserver(sock,msg):
-    packet = pickle.dumps(message(msg))
-    length = struct.pack('!I', len(packet))
-    packet = length + packet
-    sock.send(packet)
+def exec_message(msg, conn):
+    name = "n.a."
+    for b in BUDDYS:
+        if b.conn == conn:
+            name = b.name
+    print(str(name) + ": " + str(msg))
 
 
-def sendquittoserver(sock):
-    packet = pickle.dumps(exit())
-    length = struct.pack('!I', len(packet))
-    packet = length + packet
-    sock.send(packet)
+def exec_exit(conn):
+    i = 0
+    for b in BUDDYS:
+        if b.conn == conn:
+            b.conn.close()
+            del BUDDYS[i]
+            break
+        i += 1
 
 
-def recjoin():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('141.37.168.38', PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    buf = b''
-    while len(buf) < 4:
-        buf += conn.recv(4 - len(buf))
-    length = struct.unpack('!I', buf)[0]
-    msg = conn.recv(length)
-    data_variable = pickle.loads(msg)
-    if type(data_variable) is join:
-        name = data_variable.name
-        print("Recjoin gets name "+name)
-        return name
+def listen_all():
+    while THREAD_RUNNING:
+        (conn, addr) = sock.accept()
+        start_new_thread(handle_connection, (conn, addr))
+    sock.close()
 
 
-def connect(s):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    try:
-        ip = '141.37.168.' + str(s)
-        print("Try to connect to ip:"+ip)
-        sock.connect((ip, 50000))
-        jointoserver(sock)
-        # b_name = recjoin()
-        # lock.acquire()
-        # buddylist.append((b_name, ip, sock))
-        # lock.release()
-        #print('IP:' + ip + ' Nickname:' + b_name)
-    except:
-        print("Connection to " + ip + " failed")
-
-
-#<editor-fold desc="Bedienelemente">
-def scan(s):
-    for i in range(s[0], s[1], 1):
-        connect(i)
-
-
-def list(l):
-    for i in l:
-        print("Nickname: "+i[0] + " is reachable under the IP: " + i[1])
-
-
-def chat(c):
-    msg = input("Enter  Message: ")
-    sendmessagetoserver(c, msg)
-
-
-def group_chat(g):
-    for i in buddylist:
-        sendmessagetoserver(i[2], g)
-
-
-def quit(q):
-    for i in q:
-        sendquittoserver(i[2])
-        i[2].close()
-        buddylist.remove(i)
-#</editor-fold>
-
-def client():
-    o = input("Type q for quit\n Type c for chat \n Type g for group chat \n Type l for list\n Type s for scan \n")
-    if o == 'q':
-        quit(buddylist)
-    elif o == 'c':
-        buddy = input("Type in the Name of the buddy to send a message only to him\n")
-        sock = getSockofBuddy(buddy)
-        if sock is None:
-            print("Buddy isn't in the buddy list\n")
-        else:
-           chat(sock)
-    elif o == 'l':
-        list(buddylist)
-    elif o == 's':
-        scan([37, 40])
-    elif o == 'g':
-        msg = input("Enter Message")
-        group_chat(msg)
-    client()
-
-def jointoserver(sock):
-    try:
-        packet = pickle.dumps(join(name))
-        length = struct.pack('!I', len(packet))
-        packet = length + packet
-        sock.send(packet)
-    except:
-       print("IP " + sock + " refused the connection")
-
-
-def recmessage(newsocket, addr):
-    while True:
+def handle_connection(conn, addr):
+    while THREAD_RUNNING:
         buf = b''
-        print(addr[0])
         while len(buf) < 4:
-            buf += newsocket.recv(4 - len(buf))
+            try:
+                time.sleep(1)
+                buf += conn.recv(4 - len(buf))
+            except WindowsError:
+                pass
+        print("recv Message")
         length = struct.unpack('!I', buf)[0]
-        msg = newsocket.recv(length)
-        data_variable = pickle.loads(msg)
-        if type(data_variable) is join:
-            name = data_variable.name
-            print("Server gets name "+name)
-            if not getBuddyfromIp(addr[0]):
-                lock.acquire()
-                buddylist.append((name, addr[0], newsocket))
-                lock.release()
-                #jointoserver(conn)
-        elif type(data_variable) is message:
-            msg = data_variable.msg
-            print(name + ":" + msg)
-        # elif(type(data_variable) is exit):
 
+        text = conn.recv(length)
+
+        text = pickle.loads(text)
+        if type(text) is join:
+            exec_join(text.name, conn, addr)
+        elif type(text) is exit:
+            exec_exit(conn)
+        elif type(text) is message:
+            exec_message(text.msg, conn)
         else:
-            print("unknown message typ")
-
-
-def next_server(parentsocket):
-    newsocket, addr = parentsocket.accept()
-    print("Server started")
-    start_new_thread(next_server, (parentsocket,))
-    print('Connected by', addr)
-    recmessage(newsocket, addr)
+            print("recv message is not valid\n")
 
 
 
-def server():
-    HOST = '141.37.168.38'
-    PORT = 50000
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind((HOST, PORT))
-    serversocket.listen(12)
-    start_new_thread(next_server,(serversocket,))
+def scan_open():
+    try:
+        for ip in IPS:
+            scan_sock = socket(AF_INET, SOCK_STREAM)
+            result = scan_sock.connect_ex((ip, PORT))
+            if result == 0:
+                print("connect to ip: " + str(ip))
+                send_join(join(USERNAME), (ip, PORT))
+            scan_sock.close()
 
-server()
+    except gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
 
-client()
-
-# start_new_thread(server,())
-# gui()
-# client()
-# connect()
+    except error:
+        print("Couldn't connect to server")
+        sys.exit()
 
 
-#
-# s.send(message.encode('utf-8'))
-# pickledResponse = s.recv(4096);
-# response = pickle.loads(pickledResponse)
-#
-#
-# response = pickle.loads(pickledResponse)
-# EOFError: Ran out of input
+def send_join(packet, addr):
+    sock_remote = socket(AF_INET, SOCK_STREAM)
+    packet = pickle.dumps(packet)
+    length = struct.pack('!I', len(packet))
+    packet = length + packet
+    sock_remote.connect(addr)
+    tmp = Buddy(USERNAME, sock_remote, addr)
+    if tmp not in BUDDYS:
+        sock_remote.send(packet)
+        buf = b''
+        while len(buf) < 4:
+            try:
+                time.sleep(0.2)
+                buf += sock_remote.recv(4 - len(buf))
+            except WindowsError:
+                pass
+        length = struct.unpack('!I', buf)[0]
+
+        text = sock_remote.recv(length)
+
+        text = pickle.loads(text)
+
+        if type(text) is join:
+            tmp = Buddy(text.name, sock_remote, addr)
+            BUDDYS.append(tmp)
+            print(str(tmp.name) + " connected.")
+
+
+def send_packet_server(packet, conn):
+    packet = pickle.dumps(packet)
+    length = struct.pack('!I', len(packet))
+    packet = length + packet
+    conn.send(packet)
+
+
+start_new_thread(listen_all, ())
+
+
+print("client starting")
+
+while True:
+    text = input("Command ? ('help' for Commands)\n")
+    if text == "Q":
+        for b in BUDDYS:
+            send_packet_server(exit(), b.conn)
+            b.conn.close()
+
+        THREAD_RUNNING = False
+        break
+    elif text == "help":
+            print("S------------Scan for Connections  \n")
+            print("L------------List connected Buddys\n")
+            print("C------------Send a Message to one   \n")
+            print("Q------------Quit                                     \n")
+            print("S------------Scan for Connections   \n")
+            print("G------------Same as no Command         \n\n")
+
+    elif text == "S":
+        scan_open()
+    elif text == "L":
+        for b in BUDDYS:
+            print("Buddy with name: " + str(b.name) + " has Address: " + str(b.addr) + "\n")
+    elif text == "C":
+        toWhichNickname = input("To which chat you wish to send a message ?\n")
+        for b in BUDDYS:
+            if toWhichNickname == b.name:
+                msg = input("Enter your Message:\n")
+                send_packet_server(message(msg), b.conn)
+                break
+    elif text == "G":
+        print("Send Broadcast Message\n")
+        msg = input("Please enter your message:\n")
+        for b in BUDDYS:
+            send_packet_server(message(msg), b.conn)
+    else:
+        print("None Command")
+        continue
