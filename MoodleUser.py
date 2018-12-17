a46c73553aa46c6004dc261b5fe9430a11459607
ThimoSchaub <32570143+ThimoSchaub@users.noreply.github.com>
2018-12-17 09:48:25 +0100

    Update MoodleUser.py

--- MoodleUser.py
+++ MoodleUser.py
@@ -1,15 +1,17 @@
-import http.client, zlib,gzip
+import http.client, zlib, gzip
 import urllib.parse
+
 conn = http.client.HTTPSConnection("moodle.htwg-konstanz.de")
 
+
 def login():
-    #get first cookie
-    conn.request('GET','/moodle/')
+    # get first cookie
+    conn.request('GET', '/moodle/')
     r1 = conn.getresponse()
     tempcookie = r1.getheader('Set-Cookie').split(';')[0]
     r1.read()
 
-    #post password and username
+    # post password and username
     params = urllib.parse.urlencode({'username': 'rnetin', 'password': 'ntsmobil'})
     header = {'Host': 'moodle.htwg-konstanz.de',
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
@@ -20,13 +22,13 @@ def login():
               'Content-Type': 'application/x-www-form-urlencoded',
               'Content-Length': 33,
               'Connection': 'keep-alive',
-              'Cookie':tempcookie}
-    conn.request("POST", "/moodle/login/index.php",body= params, headers=header)
+              'Cookie': tempcookie}
+    conn.request("POST", "/moodle/login/index.php", body=params, headers=header)
     r2 = conn.getresponse()
     print(r2.status, r2.reason)
     global cookie
     cookie = r2.getheader('Set-Cookie').split(';')[0]
-    #aprove the test
+    # aprove the test
     r2.read()
     params = urllib.parse.urlencode({'testsession': 16509})
     header = {'Host': 'moodle.htwg-konstanz.de',
@@ -41,9 +43,9 @@ def login():
     conn.request('GET', '/moodle/login/index.php?testsession=16509', body=params, headers=header)
     r3 = conn.getresponse()
     r3.read();
-    
+
     print(r3.status, r3.reason)
-    params = urllib.parse.urlencode({'id':2279})
+    params = urllib.parse.urlencode({'id': 2279})
     header = {'Host': 'moodle.htwg-konstanz.de',
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -58,10 +60,9 @@ def login():
     r4.read()
     print(r4.status, r4.reason)
 
-def download():
 
-    
-    params = urllib.parse.urlencode({'id':118815})
+def download():
+    params = urllib.parse.urlencode({'id': 118815})
     header = {'Host': 'moodle.htwg-konstanz.de',
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -75,9 +76,9 @@ def download():
     r5 = conn.getresponse()
     r5.read()
     print(r5.status, r5.reason)
-    
-    #download file
-    params = urllib.parse.urlencode({'forcedownload':1})
+
+    # download file
+    params = urllib.parse.urlencode({'forcedownload': 1})
     header = {'Host': 'moodle.htwg-konstanz.de',
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -87,18 +88,59 @@ def download():
               'Connection': 'keep-alive',
               'Cookie': cookie,
               'Upgrade-Insecure-Requests': 1}
-    conn.request('GET', '/moodle/pluginfile.php/188750/mod_assign/introattachment/0/AIN%20RN%20-%20Laboraufgabe%20-%20HTTP.pdf?forcedownload=1', body=params, headers=header)
+    conn.request('GET',
+                 '/moodle/pluginfile.php/188750/mod_assign/introattachment/0/AIN%20RN%20-%20Laboraufgabe%20-%20HTTP.pdf?forcedownload=1',
+                 body=params, headers=header)
     r6 = conn.getresponse()
-    newfile = open('C:\\Users\\Thimo\\Desktop\\Rechnernetze.pdf','wb')
+    newfile = open('C:\\Users\\Thimo\\Desktop\\Rechnernetze.pdf', 'wb')
     newfile.write(r6.read())
     print(newfile)
     print(r6.status, r6.reason)
+
+
 def read():
     print('read')
+
+
 def writt():
-    print('writ')
+    params = urllib.parse.urlencode({'message': 'hallo', 'id': 183})
+    header = {'Host': 'moodle.htwg-konstanz.de',
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/64.0',
+              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+              'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+              'Accept-Encoding': 'gzip, deflate, br',
+              'Referer': 'https://moodle.htwg-konstanz.de/moodle/mod/chat/gui_basic/index.php?id=183',
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Connection': 'keep-alive',
+              'Conntent-Length':60,
+              'Cookie': cookie,
+              'Upgrade-Insecure-Requests': 1}
+    conn.request("POST", "/moodle/mod/chat/gui_basic/index.php", body=params, headers=header)
+    print('test')
+    r2 = conn.getresponse()
+    print('test2')
+    r2.read()
+    print(r2.getheaders())
+    print(r2.status, r2.reason)
+    params = urllib.parse.urlencode({})
+    header = {'Host': 'moodle.htwg-konstanz.de',
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
+              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+              'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+              'Accept-Encoding': 'gzip, deflate, br',
+              'Referer': 'https://moodle.htwg-konstanz.de/moodle/',
+              'Connection': 'keep-alive',
+              'Cookie': cookie,
+              'Upgrade-Insecure-Requests': 1}
+    conn.request('GET', r2.getheader('Location')[:], body=params, headers=header)
+    r3 = conn.getresponse()
+    r3.read()
+
+    print(r3.status, r3.reason)
+
+
 def chat():
-    params = urllib.parse.urlencode({'id':128994})
+    params = urllib.parse.urlencode({'id': 128994})
     header = {'Host': 'moodle.htwg-konstanz.de',
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -112,7 +154,7 @@ def chat():
     r5 = conn.getresponse()
     r5.read()
     print(r5.status, r5.reason)
-    params = urllib.parse.urlencode({'id':183})
+    params = urllib.parse.urlencode({'id': 183})
     header = {'Host': 'moodle.htwg-konstanz.de',
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -123,11 +165,47 @@ def chat():
               'Upgrade-Insecure-Requests': 1}
     conn.request('GET', '/moodle/mod/chat/gui_basic/index.php?id=183', body=params, headers=header)
     r5 = conn.getresponse()
-    print(r5.read())
+    data = zlib.decompressobj(0,r5.read())
+    print(data)
     print(r5.status, r5.reason)
-    
-    
+    #send message
+    params = urllib.parse.urlencode({'message': 'hallo', 'id': 183})
+    header = {'Host': 'moodle.htwg-konstanz.de',
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/64.0',
+              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+              'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+              'Accept-Encoding': 'gzip, deflate, br',
+              'Referer': 'https://moodle.htwg-konstanz.de/moodle/mod/chat/gui_basic/index.php?id=183',
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Connection': 'keep-alive',
+              'Cookie': cookie,
+              'Upgrade-Insecure-Requests': 1}
+    conn.request("POST", "/moodle/mod/chat/gui_basic/index.php", body=params, headers=header)
+    print('test')
+    r2 = conn.getresponse()
+    print('test2')
+    r2.read()
+    print(r2.getheaders())
+    print(r2.status, r2.reason)
+    params = urllib.parse.urlencode({})
+    header = {'Host': 'moodle.htwg-konstanz.de',
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
+              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+              'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+              'Accept-Encoding': 'gzip, deflate, br',
+              'Referer': 'https://moodle.htwg-konstanz.de/moodle/',
+              'Connection': 'keep-alive',
+              'Cookie': cookie,
+              'Upgrade-Insecure-Requests': 1}
+    conn.request('GET', r2.getheader('Location')[:], body=params, headers=header)
+    r3 = conn.getresponse()
+    r3.read()
+
+    print(r3.status, r3.reason)
+
+
+
 if __name__ == '__main__':
     login()
-    #download()
+    # download()
     chat()
